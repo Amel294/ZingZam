a381d68101c0a18bcc9746dd1f894d81a12c66d6
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-04-22 14:44:13 +0530

    feat(frontend): Add MainLogo component from SVG
    - Created a new MainLogo component from SVG
    - Added the MainLogo component for reuse in the project
    
    This addition was originally intended to be included in the previous commit but was missed.

---
+++ frontend/public/icons/MainLogo.jsx
@@ -0,0 +1,22 @@
+const MainLogo = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    height={25}
+    
+    viewBox="0 0 40 35"
+    {...props}
+  >
+    <path
+      fill="#7F27FF"
+      fillRule="evenodd"
+      d="M19.547 6.121H0L3.823 0h27.389l-9.95 16.587-4.382 7.305-3.008 5.016h7.724l3.032-5.016 4.416-7.305h8.473l-7.527 12.32H40v5.688H2.483L19.547 6.12Z"
+      clipRule="evenodd"
+    />
+    <path
+      fill="#7F27FF"
+      d="M29.044 16.587h-7.781l-4.383 7.305h7.748l4.416-7.305Z"
+    />
+  </svg>
+)
+export default MainLogo
