3083e17852f7ff8b0bc96db2583b3b5a0df5e3e4
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-04-25 15:08:36 +0530

    refactor(frontend): Refactor left side of login, signup, and forgot password pages
    This commit introduces a shared component called 'AuthPageLeftSideName' to refactor the left side of the login, signup, and forgot password pages. By using this shared component, code redundancy is reduced, leading to cleaner and more maintainable code. The 'AuthPageLeftSideName' component encapsulates the common UI elements present on the left side of these pages, promoting code reusability.

---
+++ frontend/src/pages/user/AuthPageLeftSideName.jsx
@@ -0,0 +1,11 @@
+const  AuthPageLeftSideName = ()=> {
+    return (<div className="md:col-span-1 order-1  flex justify-center items-center">
+        <div className="flex flex-col items-center">
+            <h1 className="text-3xl font-bold flex flex-col gap-4">
+                <span className="text-9xl md:text-6xl tracking-wide text-secondary-400">Zing Zam</span>
+                <span className="md:text-4xl tracking-tight">The Social Gamer</span>
+            </h1>
+        </div>
+    </div>);
+}
+export default AuthPageLeftSideName
