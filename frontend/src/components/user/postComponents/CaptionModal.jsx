c0e0b39c46e0e76d1b410eba076a3e700316db86
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-04-29 09:51:33 +0530

    Refactor(frontend): Integrate Redux for updating caption in CaptionModal component
    This commit refactors the CaptionModal component to integrate Redux for updating the caption. It introduces the updateCaption action from the postsSlice and dispatches it upon successfully changing the caption via an API call. Additionally, it enhances the handling of success and error messages using toast notifications for a better user experience.

--- frontend/src/components/user/postComponents/CaptionModal.jsx
+++ frontend/src/components/user/postComponents/CaptionModal.jsx
@@ -2,14 +2,21 @@ import { Modal, ModalContent, ModalHeader, ModalFooter, Button, Input } from "@n
 import AxiosWithBaseURLandCredentials from "../../../axiosInterceptor";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { updateCaption } from "../../../store/auth/postsSlice";
+import { useDispatch } from "react-redux";
 
-export default function CaptionModal({ handleClose,setIsCaptionOpen, isCaptionOpen, postId, captionText }) {
+export default function CaptionModal({ handleClose, setIsCaptionOpen, isCaptionOpen, postId, captionText }) {
     const [caption, setCaption] = useState(captionText);
-
+    const dispatch = useDispatch();
     const changeCaption = async () => {
         try {
-            console.log('Req for change caption')
             const response = await AxiosWithBaseURLandCredentials.put('/post/change-caption', { postId, caption });
+            if(response.data.success) {
+                toast.success("Caption changed successfully");
+                dispatch(updateCaption({postId,caption}))
+            }else{
+                toast.error("Caption not changed");
+            }
             console.log(response.data);
             handleClose()
         } catch (error) {
@@ -22,13 +29,13 @@ export default function CaptionModal({ handleClose,setIsCaptionOpen, isCaptionOp
             <ModalContent>
                 <ModalHeader className="w-full flex text-center">Change caption</ModalHeader>
                 <Input
-                size="md"
+                    size="md"
                     label="Enter new caption"
                     value={caption}
                     onChange={(e) => setCaption(e.target.value)}
                 />
                 <ModalFooter>
-                    <Button color="default"  onClick={changeCaption}  >Update</Button>
+                    <Button color="default" onClick={changeCaption}  >Update</Button>
                     <Button color="danger" variant="ghost" onClick={handleClose}>Cancel</Button>
                 </ModalFooter>
             </ModalContent>
