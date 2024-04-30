d94b7d4d9f87be621f1b6b185a75c5a8f6a56a09
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-04-30 15:23:01 +0530

    style(frontend): Improve user left side component UI
    Refactor the UserleftSide component to enhance UI consistency and readability. Changes include replacing the img tag with the Avatar component for user profile images, adjusting alignment for better visual presentation, and updating the Chip component styling.
    
    Changes Made:
    - Replaced the img tag with the Avatar component to display user profile images.
    - Adjusted alignment and styling of the user left side component for improved visual consistency.
    - Updated the Chip component to match the design specifications.

--- frontend/src/components/user/home/homeLeft/UserleftSide.jsx
+++ frontend/src/components/user/home/homeLeft/UserleftSide.jsx
@@ -1,8 +1,8 @@
-import { Card,  Chip,  Button } from "@nextui-org/react";
+import { Card, Chip, Button, Avatar } from "@nextui-org/react";
 import { useSelector } from "react-redux";
 import SavedIcon from "../../../../../public/icons/SavedIcon";
 import FriendsIcon from "../../../../../public/icons/FriendsIcon";
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function UserleftSide() {
     const navigate = useNavigate()
@@ -11,8 +11,8 @@ function UserleftSide() {
         <>
             <div className="w-[300px]">
                 <Card className="pb-2">
-                    <div className=" w-full bg-secondary-400 flex justify-center p-2 hover:bg-secondary-300 cursor-pointer  gap-2">
-                        <img src={userDetails.picture || "https://via.placeholder.com/150"} className="rounded-full w-8 " />
+                    <div className=" w-full bg-secondary-400 flex items-center justify-center p-2 hover:bg-secondary-300 cursor-pointer  gap-2">
+                        <Avatar showFallback name={userDetails?.name} alt="nextui logo" height={40} radius="full" src={userDetails?.picture} />
                         <Chip className=" w-full text-md" variant="light" onClick={() => navigate("/profile/" + userDetails.username)} >
                             {userDetails.name}
                         </Chip>
