8cac653e6ac0f890f7e825a8e9c28090628f80f9
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-05-04 16:09:17 +0530

    refactor(frontend): Update ContentModeration component
    - Added navigation functionality
    - Implemented navigation using useNavigate hook from react-router-dom
    - Added cursor pointer on hover for better UX

--- frontend/src/pages/admin/ContentModeration.jsx
+++ frontend/src/pages/admin/ContentModeration.jsx
@@ -1,18 +1,21 @@
 import SidebarAdmin from "../../components/admin/SidebarAdmin"
 import ContentModerationTable from "../../components/admin/ContentModeration/ContentModerationTable"
+import { useNavigate } from "react-router-dom";
 
 function ContentModeration() {
+    const navigate = useNavigate();
+
     return (
         <div className="flex ">
             <SidebarAdmin />
             <div className="w-full">
                 <div className="space-y-1 text-left pt-6 pl-9">
-                    <h4 className="text-2xl font-medium">Content Moderation </h4>
-                    <p className="text-small text-default-400">See reports from users and take action.</p>
+                    <h4 className="text-2xl font-medium hover:cursor-pointer" onClick={()=>navigate('/admin')}>Content Moderation </h4>
+                    <p className="text-small text-default-400 hover:cursor-pointer"  onClick={()=>navigate('/usermanagement')}>See reports from users and take action.</p>
                 </div>
-                < ContentModerationTable/>
+                < ContentModerationTable />
             </div>
-        </div>
+        </div >
     )
 }
 
