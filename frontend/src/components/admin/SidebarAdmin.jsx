9ffbc5da085ae10a8a01f499bea5695747f33cf0
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-05-04 15:04:09 +0530

    refactor(frontend): Update SidebarAdmin component
    - Implemented navigation using useNavigate hook from react-router-dom
    - Updated User Management link to navigate to '/admin' route
    - Updated Content Moderation link to navigate to '/contentmodration' route
    - Added cursor pointer on hover for better UX

--- frontend/src/components/admin/SidebarAdmin.jsx
+++ frontend/src/components/admin/SidebarAdmin.jsx
@@ -1,7 +1,10 @@
+import { useNavigate } from 'react-router-dom';
 import ContentModeration from '../../../public/icons/admin/sideBar/ContentModeration';
 import UserManagementIcon from '../../../public/icons/admin/sideBar/UserManagementIcon';
 import zingZamLogo from '/icons/ZingZamLogo.svg'
 const SidebarAdmin = () => {
+    const navigate = useNavigate();
+
     return (
         <aside className="flex flex-col w-64 h-screen px-5 py-8 overflow-y-auto">
             <div className='flex items-center gap-2 text-sm'>
@@ -12,12 +15,12 @@ const SidebarAdmin = () => {
                 <nav className="-mx-3 space-y-6 ">
                     <div className="space-y-3 ">
 
-                        <div className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700" >
+                        <div className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700 hover:cursor-pointer" onClick={() => navigate('/admin')} >
                             <UserManagementIcon className="h-5"/>
                             <span className="mx-2 text-sm font-medium">User Management</span>
                         </div>
 
-                        <div className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
+                        <div className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700 hover:cursor-pointer" onClick={() => navigate('/contentmodration')}>
                             <ContentModeration className="h-5"/>
 
                             <span className="mx-2 text-sm font-medium">Content Moderation</span>
