f8186a40bd8393e9e7b2243a26f42cf546082e46
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-05-10 16:49:36 +0530

    feat(backend): Add new file for coin packs data
    Added a new file named "coinPacks.js" to the static directory containing data for different coin packs.

---
+++ backend/static/coinPack.js
@@ -0,0 +1,29 @@
+const coinPacks =[
+    {
+        "name": "Starter Pack",
+        "coins": 100,
+        "price": 135
+    },
+    {
+        "name": "Basic Pack",
+        "coins": 480,
+        "price": 648
+    },
+    {
+        "name": "Value Pack",
+        "coins": 1050,
+        "price": 1417.50
+    },
+    {
+        "name": "Premium Pack",
+        "coins": 2800,
+        "price": 3360
+    },
+    {
+        "name": "Ultimate Pack",
+        "coins": 6000,
+        "price": 7200
+    }
+];
+
+module.exports = coinPacks 
