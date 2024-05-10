cbaca8e92aeff0020048160b398f664645c282e6
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-05-10 16:50:58 +0530

    feat(backend): Add ZingCoins schema model
    Added a new file named "ZingCoins.js" containing the Mongoose schema model for ZingCoins.

---
+++ backend/models/ZingCoins.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const ZingCoinsSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true
+    },
+    coins: {
+        type: Number,
+        required: true
+    },
+    purchases: [{
+        coinPackName: { type: String },
+        purchaseAmount: { type: Number },
+        razorpayOrderId:{type:String},
+        timestamp: { type: Date, default: Date.now }
+    }],
+    donationsSend: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    donationsReceived: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('ZingCoins', ZingCoinsSchema);
