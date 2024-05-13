4a2354bb3ada8acade5055e636582b456a2fccd2
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-05-13 16:53:52 +0530

    refactor(frontend): updated videoPlayer Component
    - Added style.css
    - Added PlayIcon

--- frontend/src/components/user/gaming/VideoPlayer.jsx
+++ frontend/src/components/user/gaming/VideoPlayer.jsx
@@ -1,38 +1,49 @@
-import { useState } from 'react';
-import { ReactFlvPlayer } from 'react-flv-player';
+import React, { useState } from 'react';
+import ReactPlayer from 'react-player';
+import './style.css';
+import PlayIcon from '../../../../public/icons/PlayIcon';
 
-const VideoPlayer = ({streamKey}) => {
-  const [reconnectAttempts, setReconnectAttempts] = useState(0);
-  const maxReconnectAttempts = 3;
+const VideoPlayer = ({ streamKey }) => {
+  const [isBuffering, setIsBuffering] = useState(false);
 
-  const handlePlayerError = (err) => {
-    switch (err) {
-      case 'NetworkError':
-      case 'MediaError':
-        if (reconnectAttempts < maxReconnectAttempts) {
-          setReconnectAttempts(reconnectAttempts + 1);
-        } else {
-          console.log('Max reconnect attempts reached');
-        }
-        break;
-      default:
-        console.log('Other error');
-    }
+  const playerStyle = {
+    position: 'relative',
+    width: '100%',
+    height: '100%',
+  };
+
+  const controlsStyle = {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    borderRadius: '5px',
+    padding: '10px',
+  };
+
+  const controlButtonStyle = {
+    color: 'white',
+    cursor: 'pointer',
   };
 
   return (
-    <div className='w-full h-auto rounded-lg'>
-      <ReactFlvPlayer
-        type="flv"
+    <div className='video-player-container hover:cursor-pointer' style={playerStyle}>
+      {isBuffering && <div className="buffering">Loading...</div>}
+      <ReactPlayer
         url={`http://localhost:7000/live/${streamKey}.flv`}
-        isLive={true}
-        isMuted={true}
-        enableStashBuffer={false}
-        handleError={handlePlayerError}
-        enableError={true}
-        reconnectInterval={5000}
-        autoPlay={true}
-        key={reconnectAttempts} 
+        playing
+        controls
+        width='100%'
+        height='100%'
+        style={controlsStyle}
+        onBuffer={() => setIsBuffering(true)}
+        onBufferEnd={() => setIsBuffering(false)}
+        onError={(e) => console.log('Error:', e)}
+        pip = {true}
+        muted={false}
+        config={{
+          file: { forceFLV: true },
+        }}
+        controlsList="nodownload"
+        light={true}
+        playIcon={<button className=' p-4 rounded-full' style={controlButtonStyle}><PlayIcon className='opacity-50 hover:opacity-100'/></button>}
       />
     </div>
   );
