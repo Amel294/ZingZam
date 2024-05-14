8688a46b9007863ad390b3b58d660a1763c0625c
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-05-14 18:32:31 +0530

    refactor(backend): update socket server to remove database integration

--- backend/socketServer.js
+++ backend/socketServer.js
@@ -1,31 +1,32 @@
 const socketIo = require("socket.io");
-const addMessageToStreamChat = require("./socketHelper/addMessageToStreamChat");
+
 function initializeSocketServer(server) {
-  const io = socketIo(server, {
-    cors: {
-      origin: process.env.BASE_URL_FRONTEND,
-      methods: ["GET", "POST"],
-      allowedHeaders: ["my-custom-header"],
-      credentials: true
-    }
-  });
-
-  io.on("connection", (socket) => {
-
-    socket.on('join room', (streamKey) => {
-      socket.join(streamKey);
-      console.log(`A user joined room: ${ streamKey }`);
+    const io = socketIo(server, {
+        cors: {
+            origin: process.env.BASE_URL_FRONTEND,
+            methods: ["GET", "POST"],
+            allowedHeaders: ["my-custom-header"],
+            credentials: true
+        }
     });
 
-    socket.on("chat message", async ({ msg, room, senderId }) => {
-      addMessageToStreamChat(msg,room,senderId)
-      io.to(room).emit("chat message", msg);
-      console.log(`Message sent to room ${ room }: ${ msg }`);
-    });
+    io.on("connection", (socket) => {
 
-    socket.on("disconnect", () => {
-      console.log("Client disconnected");
+        socket.on('join room', (streamKey) => {
+            socket.join(streamKey);
+            console.log(`A user joined room: ${streamKey}`);
+        });
+
+        socket.on("chat message", async ({ currentUserName,text, room, senderId }) => {
+            const message = {currentUserName, text, room, senderId };
+            io.to(room).emit("chat message", message);
+            console.log(`Message sent to room ${room}: ${text}`);
+        });
+
+        socket.on("disconnect", () => {
+            console.log("Client disconnected");
+        });
     });
-  });
 }
-module.exports = initializeSocketServer
+
+module.exports = initializeSocketServer;
