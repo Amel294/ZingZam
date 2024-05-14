a0f59a6d953183d86c7e0ad580c6ff0496039a94
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-05-14 18:21:51 +0530

    refactor(frontend) : PlayIcon size reduced

--- frontend/public/icons/PlayIcon.jsx
+++ frontend/public/icons/PlayIcon.jsx
@@ -1,8 +1,8 @@
 const PlayIcon = (props) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
-        width={300}
-        height={300}
+        width={150}
+        height={150}
         fill="white"
         viewBox="0 0 24 24"
         {...props}
