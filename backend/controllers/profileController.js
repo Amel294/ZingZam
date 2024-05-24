8d66506cff9876e9be0c3c407b724b51cf60dda1
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-05-24 23:21:02 +0530

    feat(backend): Add route and controller function for updating user bio

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -4,8 +4,8 @@ exports.getProfile = async (req, res) => {
     const id = req?.userData?.id;
     const requestedUserName = req.params.username.trim().toLowerCase().replace(/\s/g, '')
     const userData = await UserModel.findOne({ username: requestedUserName })
-    if(!userData){
-        return res.status(200).json({notFound: true});
+    if (!userData) {
+        return res.status(200).json({ notFound: true });
     }
     const ownProfile = userData._id.toString() === id
     const connection = await ConnectionModel.findOne({ user: id }).populate('friends')
@@ -40,7 +40,7 @@ exports.updateName = async (req, res) => {
 exports.updateUserName = async (req, res) => {
     try {
         const userId = req?.userData?.id;
-        const  username  = req.body.username.trim().toLowerCase().replace(/\s/g, '');
+        const username = req.body.username.trim().toLowerCase().replace(/\s/g, '');
         const user = await UserModel.findByIdAndUpdate(userId, { username }, { new: true });
         res.status(200).json({
             message: "Username updated successfully",
@@ -50,3 +50,20 @@ exports.updateUserName = async (req, res) => {
         res.status(500).json({ error: "Internal server error" });
     }
 };
+
+exports.addOrChangeBio = async (req, res) => {
+    try {
+        const userId = req?.userData?.id;
+        const bio = req.body.bio
+        const user = await UserModel.findByIdAndUpdate(userId, { bio }, { new: true });
+        if (!user) {
+            return res.status(200).json({ notFound: true });
+        }
+        res.status(200).json({
+            message: "Bio updated successfully",
+            bio: user.bio,
+        });
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getProfile, updateName,updateUserName } = require('../controllers/profileController');
+const { getProfile, updateName,updateUserName,addOrChangeBio } = require('../controllers/profileController');
 const router = express.Router()
 
 router.get('/:username',getProfile );
 router.post("/update-name",updateName)
 router.post("/update-username",updateUserName)
+router.post("/add-or-change-bio",addOrChangeBio)
 
 module.exports = router;
