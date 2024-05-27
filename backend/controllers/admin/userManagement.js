29d211076b6240a9d11bc21441b1d2183d60896b
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-05-27 19:03:49 +0530

    feat(backend): update user management routes for blocking users
    - Updated userManagement route import to include blockUser function.
    - Added POST route for blocking users with userId and reportId parameters.

--- backend/controllers/admin/userManagement.js
+++ backend/controllers/admin/userManagement.js
@@ -1,5 +1,6 @@
 
 const UserModel = require('../../models/UserModel')
+const ReportModel = require('../../models/ReportModel')
 exports.userManagement =async (req,res) =>{
     try {
         const users = await UserModel.find({role:"user"},{name : 1 ,username : 1,email : 1 ,picture:1,isBlocked:1});
@@ -22,6 +23,31 @@ exports.blockUnblock = async (req, res) => {
         user.isBlocked = !user.isBlocked;
         await user.save();
         res.status(200).json({ message: "User block/unblock status updated successfully", isBlocked: user.isBlocked });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+};
+exports.blockUser = async (req, res) => {
+    try {
+        const { userId, reportId } = req.params;
+        console.log(userId);
+        const user = await UserModel.findById(userId);
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        user.isBlocked = true;
+        await user.save();
+
+        const report = await ReportModel.findById(reportId);
+        if (!report) {
+            return res.status(404).json({ error: "Report not found" });
+        }
+        report.status = 'closed';
+        report.actionTaken = 'block';
+        await report.save();
+
+        res.status(200).json({ message: "User blocked successfully and report updated", isBlocked: user.isBlocked });
     } catch (error) {
         console.error(error);
         res.status(500).send("Internal Server Error");
--- backend/routes/admin/userManagement.js
+++ backend/routes/admin/userManagement.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { userManagement, blockUnblock } = require('../../controllers/admin/userManagement');
+const { userManagement, blockUnblock,blockUser } = require('../../controllers/admin/userManagement');
 const userModel = require('../../models/UserModel')
 const router = express.Router();
 router.get("/usermanagement", userManagement);
 router.post("/blockunblock/:userId",blockUnblock)
+router.post("/block-user/:userId/:reportId",blockUser)
 module.exports = router;
