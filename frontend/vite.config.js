b9fd029812dac58ecb98f26868429c88089e85f6
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-05-31 12:17:23 +0530

    chore(frontend) : config update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,24 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    hmr: {
+      host: 'zingzam.in',
+      port: 5173,
+    },
+    proxy: {
+      '/api': {
+        target: 'http://zingzam.in/backend',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      },
+      '/ws': {
+        target: 'http://zingzam.in/ws',
+        ws: true,
+        changeOrigin: true,
+      }
+    }
+  }
 })
