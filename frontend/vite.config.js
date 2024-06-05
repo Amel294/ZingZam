150e750f1aa182de583ffc3b7f4b74e12d958e32
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-06-05 19:09:09 +0530

    chore(frontend) : update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,18 +6,5 @@ dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    hmr: {
-      host: 'localhost.com',
-      port: 5173,
-    },
-    proxy: {
-      '/api': {
-        target: process.env.VITE_BASE_URL_BACKEND, 
-        changeOrigin: true,
-      },
-    },
-  },
+  
 })
