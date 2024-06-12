2e28b370c7172e1271e5f553a60429ab3b4b11a2
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-06-12 11:45:04 +0530

    fix (frontend) : fix issue with static avatar

--- frontend/src/components/user/postComponents/AddPost.jsx
+++ frontend/src/components/user/postComponents/AddPost.jsx
@@ -1,34 +1,26 @@
-import {  Button, useDisclosure, Card, CardBody, Divider, Image, CardHeader } from "@nextui-org/react";
-import Avatar from '/Avatar/amel.jpg'
+import { Button, useDisclosure, Card, CardBody, Divider, Image, CardHeader, Avatar } from "@nextui-org/react";
 import PhotoAndVideo from '/icons/photoAndVideo.svg'
 import Live from '/icons/live.svg'
 import Feeling from '/icons/feeling.svg'
 import AddPostModal from "./AddPostModal";
+import { useSelector } from "react-redux";
 
 export default function AddPost() {
     const { isOpen, onOpen, onOpenChange } = useDisclosure();
-
+    const ownUser = useSelector(state => state.auth);
     return (
         <>
-
             <Card className="flex grow max-w-[400px] md:max-w-[440px] md:min-w-[400px] max-h-[130px]">
                 <CardHeader className="flex gap-3" >
-                    <Image
-                        alt="nextui logo"
-                        height={30}
-                        radius="full"
-                        src={Avatar}
-                        width={30}
-                    />
+                    <Avatar src={`${ ownUser?.picture }`} showFallback />
                     <div className="flex grow ">
                         <Button radius={"lg"} size="md" fullWidth="true" onPress={onOpen}
-                        className="">How are you feeling today?Amel</Button>
+                            className="">How are you feeling today?Amel</Button>
                     </div>
                 </CardHeader>
                 <Divider />
                 <CardBody>
                     <div className="flex justify-evenly">
-
                         <Button onPress={onOpen} variant="light" className="rounded-lg">
                             <img className="h-4 " src={Live} alt="" />
                             Live
@@ -42,12 +34,9 @@ export default function AddPost() {
                             Feelings
                         </Button>
                     </div>
-
                 </CardBody>
-
             </Card>
-            <AddPostModal isOpen={isOpen} onOpenChange={onOpenChange}/>
+            <AddPostModal isOpen={isOpen} onOpenChange={onOpenChange} />
         </>
-
     );
 }
